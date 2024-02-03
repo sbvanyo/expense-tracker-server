a20de47db9e3bd07f6354191297d80b1dab4a555
David Poole <dbpoole7@gmail.com>
2024-02-03 11:00:07 -0600

    added date and description to trip model, updated trip fixtures and reran migrations to update

--- tripexpensetrackerapi/models/trip.py
+++ tripexpensetrackerapi/models/trip.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.utils import timezone
 from tripexpensetrackerapi.models import User
 class Trip(models.Model):
     name = models.CharField(max_length=51)
+    date = models.DateTimeField(default=timezone.now)
+    description = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
