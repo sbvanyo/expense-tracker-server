d0f85857faa7d24d3a621640a5e1f6455e84bb6c
David Poole <dbpoole7@gmail.com>
2024-02-03 13:30:53 -0600

    changed date time field in expense model to align with our project

--- tripexpensetrackerapi/models/expense.py
+++ tripexpensetrackerapi/models/expense.py
@@ -7,6 +7,6 @@ class Expense(models.Model):
     name = models.CharField(max_length=51)
     amount = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.TextField()
-    date = models.DateTimeField(default=timezone.now)
+    date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     trip = models.ForeignKey(Trip, on_delete=models.CASCADE, related_name='expenses', null=True, blank=True)
